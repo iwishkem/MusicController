17890358700cab5b06a26e5bbb657f63af43fe4a
iWishkem <66250015+iwishkem@users.noreply.github.com>
2025-08-03 22:12:27 +0300

    Create NotificationListener.kt

---
+++ android/app/src/main/kotlin/com/example/kemplayer/NotificationListener.kt
@@ -0,0 +1,14 @@
+package com.example.kemplayer
+
+import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
+
+class NotificationListener : NotificationListenerService() {
+    override fun onNotificationPosted(sbn: StatusBarNotification?) {
+        // İstersen burada bildirim bazlı ekstra işlem yapabilirsin
+    }
+
+    override fun onNotificationRemoved(sbn: StatusBarNotification?) {
+        // İstersen burada da bildirim silme takibi yapılabilir
+    }
+}
